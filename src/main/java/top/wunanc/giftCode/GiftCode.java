23f6912097441c4e978063e325c7f3a34eb3b738
wunanc <wunanc@163.com>
2026-03-21 16:07:45 +0800

    Merge remote repository with local project

---
+++ src/main/java/top/wunanc/giftCode/GiftCode.java
@@ -0,0 +1,17 @@
+package top.wunanc.giftCode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GiftCode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
